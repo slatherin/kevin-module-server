4d4acdd363bfa9a059f7b478caec7206e2d06356
Kevin Jacobs <frevin8r@gmail.com>
2018-06-21 20:37:11 -0700

    refactor product routes

--- server/router/index.js
+++ server/router/index.js
@@ -3,16 +3,16 @@ const { ReviewCtrl } = require('../controllers/reviewController.js');
 const { ProductCtrl } = require('../controllers/productController.js');
 
 Router.get('/reviews/:search', ReviewCtrl.get);
-Router.get('/reviews/product/:name', ProductCtrl.get);  
+Router.get('/product/:name', ProductCtrl.get);  
 
 Router.post('/reviews/', ReviewCtrl.post);
-Router.post('/reviews/product/:name', ProductCtrl.post);
+Router.post('/product/:name', ProductCtrl.post);
 
 Router.put('/reviews/:id', ReviewCtrl.put);
-Router.put('/reviews/product/:id', ProductCtrl.put);
+Router.put('/product/:id', ProductCtrl.put);
 
 Router.delete('/reviews/:id', ReviewCtrl.delete);
-Router.delete('/reviews/product/:id', ProductCtrl.delete);
+Router.delete('/product/:id', ProductCtrl.delete);
 
 module.exports = {
   Router: Router
